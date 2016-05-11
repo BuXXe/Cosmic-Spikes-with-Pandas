7cb9c8f0ebe90fbd164b0fee35696a1926b6f4b1
CSkerra <skerra.christopher@yahoo.de>
2016-05-11 14:09:08 +0200

    add splash screen

--- src/test.py
+++ src/test.py
@@ -8,7 +8,9 @@
  and code inside the callback subroutines will be round-tripped.
  The 'main' function is reserved.
 """
-
+from __future__ import with_statement   # <-- Python 2.5 ONLY
+import Tix
+import time
 from Tkinter import *
 from test_ui import Test
 import tkFileDialog
@@ -257,6 +259,53 @@ class CustomTest(Test):
 
     # END CALLBACK CODE
 
+class SplashScreen( object ):
+   def __init__( self, tkRoot, imageFilename, minSplashTime=0 ):
+      self._root              = tkRoot
+      self._image             = Tix.PhotoImage( file=imageFilename )
+      self._splash            = None
+      self._minSplashTime     = time.time() + minSplashTime
+      
+   def __enter__( self ):
+      # Remove the app window from the display
+      self._root.withdraw( )
+      
+      # Calculate the geometry to center the splash image
+      scrnWt = self._root.winfo_screenwidth( )
+      scrnHt = self._root.winfo_screenheight( )
+      
+      imgWt = self._image.width()
+      imgHt = self._image.height()
+      
+      imgXPos = (scrnWt / 2) - (imgWt / 2)
+      imgYPos = (scrnHt / 2) - (imgHt / 2)
+
+      # Create the splash screen      
+      self._splash = Tix.Toplevel()
+      self._splash.overrideredirect(1)
+      self._splash.geometry( '+%d+%d' % (imgXPos, imgYPos) )
+      Tix.Label( self._splash, image=self._image, cursor='watch' ).pack( )
+
+      # Force Tk to draw the splash screen outside of mainloop()
+      self._splash.update( )
+   
+   def __exit__( self, exc_type, exc_value, traceback ):
+      # Make sure the minimum splash time has elapsed
+      timeNow = time.time()
+      if timeNow < self._minSplashTime:
+         time.sleep( self._minSplashTime - timeNow )
+      
+      # Destroy the splash window
+      self._splash.destroy( )
+      
+      # Display the application window
+      self._root.deiconify( )
+
+
+#--------------------------------------------
+# Now putting up splash screens is simple
+
+
 root = None
 def main():
     # Standalone Code Initialization
@@ -265,13 +314,30 @@ def main():
     try: userinit()
     except NameError: pass
     global root
-    root = Tk()
-    demo = CustomTest(root)
-    root.title('Unspike with Pandas')
-    root.iconbitmap("Panda.ico")
-    try: run()
-    except NameError: pass
-    root.protocol('WM_DELETE_WINDOW', root.destroy)
-    root.mainloop()
+    #root = Tk()
+    
+    
+    
+    
+    
+    
+#     demo = CustomTest(root)
+#     root.title('Unspike with Pandas')
+#     root.iconbitmap("Panda.ico")
+#     try: run()
+#     except NameError: pass
+#     root.protocol('WM_DELETE_WINDOW', root.destroy)
+#     root.mainloop()
 
+    root = Tix.Tk( )
+
+    with SplashScreen( root, 'Pandas.gif', 3.0 ):
+        demo = CustomTest(root)
+        root.title('Unspike with Pandas')
+        root.iconbitmap("Panda.ico")
+        try: run()
+        except NameError: pass
+        root.protocol('WM_DELETE_WINDOW', root.destroy)
+    
+    root.mainloop( )
 if __name__ == '__main__': main()
