240a48ba00f63f2c38963ed0d62e6ab163a3ea90
BuXXe <skerra.christopher@yahoo.de>
2016-05-29 20:11:12 +0200

    improve manual unspiker

--- src/DataSet.py
+++ src/DataSet.py
@@ -86,15 +86,14 @@ class DataSet(object):
 
 
     def manualUnspike(self):
-        res = self.processedData.plot(title="MANUAL UNSPIKER\n"+self.filename,legend  = False,picker=2)
+        res = self.processedData.interpolate().plot(title="MANUAL UNSPIKER\nd: delete marked points , w: undo last marking\n"+self.filename,legend  = False,picker=2)
         res.figure.canvas.mpl_connect('pick_event', self.onpick3)
         res.figure.canvas.mpl_connect('key_release_event', self.onkeyhand)
 
 
     def onkeyhand(self,event):
             
-            print "test"
-            if event.key =="k":
+            if event.key =="w":
                 if len(self.toDelete) >0:
                     index = len(self.toDelete)-1
                     circ = self.toDelete[index][2]
@@ -102,26 +101,42 @@ class DataSet(object):
                     circ.remove()
                     plt.draw()
             if event.key =="d":
-                for item in self.toDelete:
-                    self.originalData[item[0]][self.originalData.index.values[item[1]]] = nan
-                self.toDelete=[]
-                axi =  plt.gca().axis()
-                plt.cla()
-                self.originalData.plot(title=self.filename,ax=plt.gca(),legend  = False,picker=1)
-                plt.axis(axi)
+                if len(self.toDelete) > 0:
+                    # first do history and paremeter history updates
+                    self.history.append(self.processedData.copy())
+                    
+                    # create list of x val and corresponding frame of to be deleted point
+                    deleteset = []
+                    # TODO better formatting: perhaps per frame sorted ? cut off / round to 2 behind the comma + SORT values ascending!
+                    for entry in self.toDelete:
+                        deleteset.append((entry[0],self.processedData.index.values[entry[1]]))
+                    
+                    self.parameterHistory.append(("Manual",deleteset))
+                    
+                    for item in self.toDelete:
+                        self.processedData[item[0]][self.processedData.index.values[item[1]]] = nan
+                    self.toDelete=[]
+                    axi =  plt.gca().axis()
+                    plt.cla()
+                    self.processedData.interpolate().plot(title="MANUAL UNSPIKER\nd: delete marked points , w: undo last marking\n"+self.filename,ax=plt.gca(),legend  = False,picker=2)
+                    plt.axis(axi)
 
                     
        
     def onpick3(self,event):
-            print event.ind
-            print "test"
+            # ensure there are no double entries!
+            for (frame,pos,circle) in self.toDelete:
+                if frame == event.artist.get_label() and pos == event.ind[0]:
+                    return
+            
             ind = event.ind
     
-            xval = self.originalData[event.artist.get_label()].iloc[ind[0]]
-            yval = self.originalData.index.values[ind[0]]
+            yval = self.processedData[event.artist.get_label()].iloc[ind[0]]
+            xval = self.processedData.index.values[ind[0]]
     
-            circle1=plt.Circle((yval,xval),5,color='r')
+            circle1=plt.Circle((xval,yval),5,color='r')
             
+            # toDelete content: Frame, xpos, circle instance
             self.toDelete.append((event.artist.get_label(),ind[0],circle1))
             
             event.artist.axes.add_artist(circle1)
